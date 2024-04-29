7c81ec3d8cbf67dc5aa9eb1eb453085ee5f3ebe6
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-04-29 16:45:03 +0700

    - add json master Data

--- django_crud_example/payroll/admin.py
+++ django_crud_example/payroll/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Department, Country
 
 # Register your models here.
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    pass
